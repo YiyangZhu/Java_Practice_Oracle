0cc332c070316f0266c0d1e285c00981560c3d41
Yiyang Zhu <ererandkeke@gmail.com>
2018-03-04 23:00:36 -0500

    demonstrates the access modifier of an interface

---
+++ Reader.java
@@ -0,0 +1,5 @@
+//the access modifier is public, so this interface can be implemented by code in any package
+//if access modifier not used, the interface is available only to other members of its package
+public interface Reader{
+    String read();
+}
