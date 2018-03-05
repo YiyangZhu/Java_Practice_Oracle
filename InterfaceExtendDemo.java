02154f4f96deae8cfd4ecf77d0bc39de3500081a
Yiyang Zhu <ererandkeke@gmail.com>
2018-03-05 12:23:27 -0500

    demonstrates extend interface

---
+++ InterfaceExtendDemo.java
@@ -0,0 +1,8 @@
+class InterfaceExtendDemo{
+    public static void main(String[] args){
+        MyClass m = new MyClass();
+        m.meth1();
+        m.meth2();
+        m.meth3();
+    }
+}
---
+++ MyClass.java
@@ -0,0 +1,16 @@
+class MyClass implements B{
+    //access modifier must be public
+    public void meth1(){
+        System.out.println("Implement meth1()!");
+    }
+    
+    //access modifier must be public
+    public void meth2(){
+        System.out.println("Implement meth2()!");
+    }
+    
+    //access modifier must be public
+    public void meth3(){
+        System.out.println("Implement meth3()!");
+    }
+}
