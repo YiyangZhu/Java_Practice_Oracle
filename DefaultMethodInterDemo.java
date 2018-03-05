2c05942b791468c1c83e79703ef835f15a8f832b
Yiyang Zhu <ererandkeke@gmail.com>
2018-03-05 16:27:13 -0500

    demonstrates default method in interface

--- DefaultMethodInterDemo.java
+++ DefaultMethodInterDemo.java
@@ -6,5 +6,8 @@ class DefaultMethodInterDemo{
         //call abstract method in interface
         System.out.println("User ID is:"+m.getUserID());
         //both works
+        MyIFImp2 m2 = new MyIFImp2();
+        System.out.println("Admin ID is:"+m2.getAdminID());
+        System.out.println("User ID is:"+m2.getUserID());
     }
 }
---
+++ MyIFImp2.java
@@ -0,0 +1,10 @@
+//implements MyIF containing default method
+class MyIFImp2 implements MyIF{
+    public int getUserID(){
+        return 10;
+    }
+    //implements the default method
+    public int getAdminID(){
+        return -10;
+    }
+}
