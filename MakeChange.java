480554b130dffead6d9c17b1b963370ce41a10f2
Yiyang Zhu <ererandkeke@gmail.com>
2018-03-15 16:31:42 -0400

    solution to make change problem using iterative way

---
+++ MakeChange.java
@@ -0,0 +1,21 @@
+class MakeChange{
+    public static void main(String[] args){
+        int input = 58;
+        System.out.println("There are "+makeChange(input)+" different ways.");
+    }
+    
+    static int makeChange(int n) {
+        int ways = 0;
+        for (int count25 = 0; count25 * 25 <= n; count25++) {
+            for (int count10 = 0; count10 * 10 <= n; count10++) {
+                for (int count5 = 0; count5 * 5 <= n; count5++) {
+                    int sum = count25*25 + count10*10 + count5*5;
+                    if (sum <= n) {
+                        ways++;
+                    }
+                }
+            }
+        }
+        return ways;
+    }
+}
