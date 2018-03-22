e2e2c867e1f070b2300b17768e4d39b64ad3c815
Yiyang Zhu <ererandkeke@gmail.com>
2018-03-22 16:13:28 -0400

    demonstrates how to use Throwable

---
+++ ExceptionDemo5.java
@@ -0,0 +1,15 @@
+class ExceptionDemo5{
+    public static void main(String[] args){
+        int[] numer = {9,27,81,243,729,2187,6561};
+        int[] denom = {3,0,9,9,0,0};
+        for(int i = 0; i < numer.length; i++){
+            try{
+                System.out.println(numer[i]+"/"+denom[i]+"="+numer[i]/denom[i]);
+            } catch (ArithmeticException ex){ // subclass must be put before super class, else will cause error - unreachable code error
+                System.out.println("Arithmetic exception! Cannot divide by zero!"); // Throwable is the superclass of ALL exceptions
+            } catch (Throwable ex){
+                System.out.println("Some exceptions occur!");
+            }
+        } 
+    }
+}
