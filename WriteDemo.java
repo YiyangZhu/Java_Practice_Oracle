2e594bb86f961255e4d836c152a6eb7a8041f619
Yiyang Zhu <ererandkeke@gmail.com>
2018-03-23 16:34:46 -0400

    demonstrate System.out.write(parameter)

---
+++ WriteDemo.java
@@ -0,0 +1,11 @@
+class WriteDemo{
+    public static void main(String[] args){
+        char a, b;
+        b = 'X';
+        a = 'x';
+        System.out.write(b);
+        System.out.write(a);
+        System.out.write('5');
+        System.out.write('\n');
+    }
+}
